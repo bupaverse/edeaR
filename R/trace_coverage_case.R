2483393e73d0c2af85f1932909e7027154119fc4
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2015-12-01 13:20:42 +0100

    Trace coverage at case level

---
+++ R/trace_coverage_case.R
@@ -0,0 +1,15 @@
+
+trace_coverage_case <- function(eventlog) {
+	library(dplyr)
+
+	stop_eventlog(eventlog)
+
+	case_classifier <- case_id(eventlog)
+
+	tr <- cases_light(eventlog) %>%
+		group_by(trace) %>%
+		mutate(absolute_trace_coverage = n(), relative_trace_coverage = absolute_trace_coverage/n_cases(eventlog)) %>%
+		arrange(desc(absolute_trace_coverage)) %>%
+		select(-trace_id)
+	return(tr)
+}
