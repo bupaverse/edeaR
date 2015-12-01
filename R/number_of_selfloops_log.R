a6d77980868223b24f89c0f7ce59e628b32f9af0
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2015-12-01 10:13:21 +0100

    Update selfloops

--- R/number_of_selfloops_log.R
+++ R/number_of_selfloops_log.R
@@ -9,7 +9,8 @@ number_of_selfloops_log <- function(eventlog) {
 	r <- summary(res$absolute)
 	r <- c(r, St.Dev = sd(res$absolute))
 	r <- c(r, IQR = r[5] - r[2])
-	names(r) <- c("min","q1","median","mean","q3","max","st_dev","iqr")
+	r <- c(r, tot = sum(res$absolute))
+	names(r) <- c("min","q1","median","mean","q3","max","st_dev","iqr", "tot")
 	r <- data.frame(r)
 	r <- as.data.frame(t(r))
 	row.names(r) <- NULL
