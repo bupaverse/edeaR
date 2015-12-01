440afca9c340e7dbd071965bfd7fa24232a2bdfc
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2015-12-01 14:26:13 +0100

    Size of selfloops case update output

--- R/size_of_selfloops_case.R
+++ R/size_of_selfloops_case.R
@@ -4,6 +4,6 @@ size_of_selfloops_case <- function(eventlog, include_non_selfloops) {
 	size_of_selfloops <- size_of_selfloops_trace(eventlog) %>% select(-relative_trace_frequency)
 	cases <- cases_light(eventlog) %>% select(-trace_id)
 
-	r <- merge(cases, size_of_selfloops) %>% select(-trace)
+	r <- merge(cases, size_of_selfloops) %>% select(-trace) %>% tbl_df
 	return(r)
 }
