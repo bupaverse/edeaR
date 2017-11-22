cd80a8d77fabcafaf460773d2fa167dde6028d72
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2017-11-22 16:22:26 +0100

    Refactoring filter activity

--- R/filter_activity.R
+++ R/filter_activity.R
@@ -19,31 +19,22 @@ filter_activity <- function(eventlog, activities, reverse) {
 #' @export
 
 filter_activity.eventlog <- function(eventlog,
-							activities,
-							reverse = FALSE){
-	mapping <- mapping(eventlog)
-
-	if(reverse == F)
-		output <- filter(eventlog, (!!as.symbol(activity_id(eventlog))) %in% activities)
-
+									 activities,
+									 reverse = FALSE){
+	if(reverse == FALSE)
+		filter(eventlog, (!!as.symbol(activity_id(eventlog))) %in% activities)
 	else
-		output <- filter(eventlog, !((!!as.symbol(activity_id(eventlog))) %in% activities))
-
-	output <- output %>% re_map(mapping)
-
-	return(output)
+		filter(eventlog, !((!!as.symbol(activity_id(eventlog))) %in% activities))
 }
 
 #' @describeIn filter_activity Filter grouped eventlog for activity labels
 #' @export
 
 filter_activity.grouped_eventlog <- function(eventlog,
-									 activities,
-									 reverse = FALSE){
+											 activities,
+											 reverse = FALSE){
 
 	grouped_filter(eventlog, filter_activity, activities, reverse)
-
-
 }
 
 
@@ -55,17 +46,18 @@ ifilter_activity <- function(eventlog) {
 		gadgetTitleBar("Filter activities"),
 		miniContentPanel(
 			fillRow(flex = c(10,1,8),
-				selectizeInput("selected_activities", label = "Select activities:", choices = eventlog %>% pull(!!as.symbol(activity_id(eventlog))) %>%
-							   	unique %>% sort, selected = NA,  multiple = T), " ",
-				radioButtons("reverse", "Reverse filter: ", choices = c("Yes","No"), selected = "No")
+					selectizeInput("selected_activities", label = "Select activities:", choices = eventlog %>% pull(!!as.symbol(activity_id(eventlog))) %>%
+								   	unique %>% sort, selected = NA,  multiple = TRUE), " ",
+					radioButtons("reverse", "Reverse filter: ", choices = c("Yes","No"), selected = "No")
 			)
 		)
 	)
-
 	server <- function(input, output, session){
 		observeEvent(input$done, {
 
-			filtered_log <- filter_activity(eventlog, activities = input$selected_activities, reverse = ifelse(input$reverse == "Yes", T, F))
+			filtered_log <- filter_activity(eventlog,
+											activities = input$selected_activities,
+											reverse = ifelse(input$reverse == "Yes", TRUE, FALSE))
 
 
 			stopApp(filtered_log)
