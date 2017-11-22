de86fb3927b9bc8343f38137521ad0e2204a3b8f
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2017-11-22 12:44:55 +0100

    Change file name activity frquency

--- R/activity_frequency_activity.R
+++ R/activity_frequency_activity.R
@@ -1,12 +1,9 @@
 
 activity_frequency_activity <- function(eventlog) {
-
-	stop_eventlog(eventlog)
-
-	r <- eventlog %>%
+	absolute <- NULL
+	eventlog %>%
 		group_by(!!as.symbol(activity_id(eventlog))) %>%
 		summarize(absolute = n_distinct(!!as.symbol(activity_instance_id(eventlog)))) %>%
 		mutate(relative = absolute/sum(absolute))
 
-	return(r)
 }
