55cffa88a28658db082dd4d15a5bb5231d127c90
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2017-11-23 13:02:22 +0100

    Refactoring filter processing time

--- R/filter_processing_time_percentile.R
+++ R/filter_processing_time_percentile.R
@@ -4,11 +4,11 @@ filter_processing_time_percentile <- function(eventlog,
 											  percentage,
 											  reverse) {
 
-	case_durations <- processing_time(eventlog = eventlog, "case") %>% arrange(processing_time)
-
-	case_selection <- case_durations %>%
-		slice(1:ceiling(nrow(case_durations)*percentage)) %>%
-		pull(1)
+	eventlog %>%
+		processing_time("case") %>%
+		arrange(processing_time) %>%
+		slice(1:ceiling(n()*percentage)) %>%
+		pull(1) -> case_selection
 
 	filter_case(eventlog, case_selection, reverse)
 }
