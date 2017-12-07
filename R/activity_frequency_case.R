69560ada580c389b5bfc8139886463bddd98d8cf
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2017-12-07 14:55:50 +0100

    Update activity frequency

--- R/activity_frequency_case.R
+++ R/activity_frequency_case.R
@@ -1,7 +1,13 @@
 activity_frequency_case <- function(eventlog) {
 	absolute <- NULL
+	relative <- NULL
+
 	eventlog %>%
-		group_by(!!as.symbol(case_id(eventlog))) %>%
-		summarize(absolute = n_distinct(!!as.symbol(activity_id(eventlog))),
-				  relative = absolute/n_distinct(!!as.symbol(activity_instance_id(eventlog))))
+		group_by(!!case_id_(eventlog), !!activity_id_(eventlog), !!activity_instance_id_(eventlog)) %>%
+		summarize() %>%
+		summarize(n = n()) %>%
+		summarize(n_instances = sum(n), n_labels = n()) %>%
+		transmute(!!case_id_(eventlog),
+				  absolute = n_labels,
+				  relative = n_labels/n_instances)
 }
