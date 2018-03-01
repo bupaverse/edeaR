f0ba71c122db51a3c2fbb7200a0eaaddaf65cf06
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2018-03-01 14:42:32 +0100

    Start activities metric with .order

--- R/start_activities_case.R
+++ R/start_activities_case.R
@@ -2,7 +2,8 @@ start_activities_case <- function(eventlog) {
 
 	eventlog %>%
 		group_by(!!case_id_(eventlog)) %>%
-		summarize(!!activity_id_(eventlog) := first(!!activity_id_(eventlog))) %>%
+		arrange(!!timestamp_(eventlog), .order) %>%
+		slice(1:1) %>%
 		select(!!case_id_(eventlog), start_activity = !!activity_id_(eventlog))
 
 }
