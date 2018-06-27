b3d3342f08a8988c2a53762f48e1afc86cdb720c
JANSSENSWILLEN Gert <gert.janssenswillen@uhasselt.be>
2018-06-27 16:53:14 -0400

    Uniform headers start and end activities metrics #17

--- R/end_activities_case.R
+++ R/end_activities_case.R
@@ -3,6 +3,6 @@ end_activities_case <- function(eventlog) {
 		group_by_case() %>%
 		arrange(!!as.symbol(timestamp(eventlog)), .order) %>%
 		summarize(!!as.symbol(activity_id(eventlog)) := last(!!as.symbol(activity_id(eventlog)))) %>%
-		select(!!as.symbol(case_id(eventlog)), end_activity = !!as.symbol(activity_id(eventlog)))
+		select(!!as.symbol(case_id(eventlog)), !!as.symbol(activity_id(eventlog)))
 
 }
--- R/start_activities_case.R
+++ R/start_activities_case.R
@@ -4,5 +4,5 @@ start_activities_case <- function(eventlog) {
 		group_by_case() %>%
 		arrange(!!as.symbol(timestamp(eventlog)), .order) %>%
 		summarize(!!as.symbol(activity_id(eventlog)) := first(!!as.symbol(activity_id(eventlog)))) %>%
-		select(!!as.symbol(case_id(eventlog)), end_activity = !!as.symbol(activity_id(eventlog)))
+		select(!!as.symbol(case_id(eventlog)), !!as.symbol(activity_id(eventlog)))
 }
