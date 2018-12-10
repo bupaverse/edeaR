e1d0f25b19ce30dae30c856d5b3b0b5be208767b
JANSSENSWILLEN Gert <gert.janssenswillen@uhasselt.be>
2018-12-10 11:47:57 +0100

    #18 filter_lifecycle

---
+++ R/filter_lifecycle.R
@@ -0,0 +1,77 @@
+#' Filter: Life cycle
+#'
+#' Filters the log based on the life cycle id
+#'
+#' The method filter_lifecycle can be used to filter on life cycle identifiers. It has an lifecycle argument,
+#' to which a vector of identifiers can be given. The selection can be negated with the reverse argument.
+#'
+#' @param eventlog The dataset to be used. Should be a (grouped) eventlog object.
+#'
+#' @param activities Character vector containing one or more life cycle identifiers.
+#' @param reverse Logical, indicating whether the selection should be reversed.
+#' @param ... Deprecated arguments.
+#'
+#' @seealso \code{vignette("filters", "edeaR")}
+#'
+#' @return When given an eventlog, it will return a filtered eventlog. When given a grouped eventlog, the filter will be applied
+#' in a stratified way (i.e. each separately for each group). The returned eventlog will be grouped on the same variables as
+#' the original event log.
+#'
+#' @export
+filter_lifecycle <- function(eventlog, lifecycle, reverse, ...) {
+	UseMethod("filter_lifecycle")
+}
+
+#' @describeIn filter_lifecycle Filter eventlog on life cycle labels
+#' @export
+
+filter_lifecycle.eventlog <- function(eventlog,
+									  lifecycle,
+									 reverse = FALSE,
+									 ...){
+	if(reverse == FALSE)
+		filter(eventlog, (!!lifecycle_id_(eventlog)) %in% lifecycle)
+	else
+		filter(eventlog, !((!!lifecycle_id_(eventlog)) %in% lifecycle))
+}
+
+#' @describeIn filter_lifecycle Filter grouped eventlog on life cycle labels
+#' @export
+
+filter_lifecycle.grouped_eventlog <- function(eventlog,
+											  lifecycle,
+											 reverse = FALSE,
+											 ...){
+
+	grouped_filter(eventlog, filter_lifecycle, lifecycle, reverse)
+}
+
+
+#' @rdname filter_lifecycle
+#' @export ifilter_lifecycle
+ifilter_lifecycle <- function(eventlog) {
+
+	ui <- miniPage(
+		gadgetTitleBar("Filter life cycle"),
+		miniContentPanel(
+			fillRow(flex = c(10,1,8),
+					selectizeInput("selected_activities", label = "Select life cycle:", choices = eventlog %>% pull(!!lifecycle_id_(eventlog)) %>%
+								   	unique %>% sort, selected = NA,  multiple = TRUE), " ",
+					radioButtons("reverse", "Reverse filter: ", choices = c("Yes","No"), selected = "No")
+			)
+		)
+	)
+	server <- function(input, output, session){
+		observeEvent(input$done, {
+
+			filtered_log <- filter_lifecycle(eventlog,
+											 lifecycle = input$selected_activities,
+											reverse = ifelse(input$reverse == "Yes", TRUE, FALSE))
+
+
+			stopApp(filtered_log)
+		})
+	}
+	runGadget(ui, server, viewer = dialogViewer("Filter Life Cycle", height = 400))
+
+}
---
+++ R/filter_lifecycle_presence.R
@@ -0,0 +1,124 @@
+#' Filter: Life cycle Presence
+#'
+#' Filters activity instances based on the presence (or absence) of life cycles
+#'
+#' This functions allows to filter activity instances that (do not) contain certain life cycle identifiers.
+#' It requires as input a vector containing one or more life cycle labels and it has a method
+#' argument. The latter can have the values all, none or one_of.
+#' \itemize{
+#' \item When set to `all`, it means
+#' that all the specified life cycle labels must be present for an activity instance to be selected
+#' \item `none` means that they are not allowed to be present.
+#' \item `one_of` means that at least one of them must be present.
+#' \item `only` means that only (a set of) these life cycle labels are allowed to be present
+#' \item `exact` means that only exactly these life cycle labels can be present (although multiple times and in random orderings)
+#' }
+#'
+#'
+#'
+#' @inherit filter_activity params references seealso return
+#'
+#' @param method Filter method. If "all", each of the life cycle labels should be present. If "one_of", at least one of them should be present. If "none", none of the life cycle labels are allowed to occur in the filtered activity instances.
+#'
+#' @export filter_lifecycle_presence
+filter_lifecycle_presence <- function(eventlog, lifecycle, method, reverse) {
+	UseMethod("filter_lifecycle_presence")
+}
+
+
+#' @describeIn filter_lifecycle_presence Filter event log on presence of life cycle labels.
+#' @export
+
+filter_lifecycle_presence.eventlog <- function(eventlog,
+											   lifecycle = NULL,
+											  method = c("all", "one_of", "none", "exact","only"),
+											  reverse = FALSE){
+	method <- match.arg(method)
+
+	eventlog %>%
+		select(!!lifecycle_id_(eventlog), !!activity_instance_id_(eventlog), force_df = T) %>%
+		unique() %>%
+		mutate(in_selection = !!lifecycle_id_(eventlog) %in% lifecycle) %>%
+		group_by(!!as.symbol(activity_instance_id_(eventlog)), in_selection) %>%
+		summarize(n = n_distinct(!!lifecycle_id_(eventlog))) %>%
+		mutate(in_selection = ifelse(in_selection, "in_", "out_")) %>%
+		spread(in_selection, n, fill = 0) -> selection
+
+	if(method == "all")
+		filter_activity_instance(eventlog, selection %>% filter(in_ == length(lifecycle)) %>% pull(1), reverse)
+	else if(method == "one_of")
+		filter_activity_instance(eventlog, selection %>% filter(in_ >= 1) %>% pull(1), reverse)
+	else if (method == "none")
+		filter_activity_instance(eventlog, selection %>% filter(in_ == 0) %>% pull(1), reverse)
+	else if (method == "exact")
+		filter_activity_instance(eventlog, selection %>% filter(in_ == length(lifecycle), out_ == 0) %>% pull(1), reverse)
+	else if (method == "only")
+		filter_activity_instance(eventlog, selection %>% filter(out_ == 0) %>% pull(1), reverse)
+
+
+
+
+	# if(method == "all") {
+	# 	eventlog %>%
+	# 		group_by_case %>%
+	# 		filter(all(activities %in% !!activity_id_(eventlog))) %>%
+	# 		ungroup_eventlog
+	#
+	# } else if (method == "one_of") {
+	# 	eventlog %>%
+	# 		group_by_case %>%
+	# 		filter(any(activities %in% !!activity_id_(eventlog))) %>%
+	# 		ungroup_eventlog()
+	# } else if (method  == "none") {
+	# 	eventlog %>%
+	# 		group_by_case %>%
+	# 		filter(!any(activities %in% !!activity_id_(eventlog))) %>%
+	# 		ungroup_eventlog
+	# 	}
+
+}
+
+#' @describeIn filter_lifecycle_presence Filter grouped event log on presence of life cycle labels.
+#' @export
+filter_lifecycle_presence.grouped_eventlog <- function(eventlog,
+													   lifecycle = NULL,
+													  method = c("all", "one_of", "none", "exact","only"),
+													  reverse = FALSE) {
+	grouped_filter(eventlog, filter_lifecycle_presence, activities, method)
+}
+
+#' @rdname filter_lifecycle_presence
+#' @export ifilter_lifecycle_presence
+
+ifilter_lifecycle_presence <- function(eventlog) {
+
+	ui <- miniPage(
+		gadgetTitleBar("Filter activities based on presence"),
+		miniContentPanel(
+			fillCol(flex = c(2,1),
+					fillRow(flex = c(10,1,8),
+							selectizeInput("selected_activities",
+										   label = "Select life cycle:",
+										   choices = eventlog %>% pull(!!as.symbol(lifecycle_id(eventlog))) %>%
+										   	unique, selected = NA,  multiple = TRUE), " ",
+							radioButtons("method", "Method: ", choices = c("All" = "all","One of"= "one_of","None" = "none", "Exact" = "exact","Only" = "only"), selected = "all")
+					),
+					"If \"all\", each of the life cycle labels should be present.
+					If \"one_of\", at least one of them should be present. If \"none\", none of the life cycle labels are allowed to occur in the filtered traces."
+			))
+	)
+
+	server <- function(input, output, session){
+		observeEvent(input$done, {
+
+			filtered_log <- filter_lifecycle_presence(eventlog,
+													 lifecyle = input$selected_activities,
+													 method = input$method)
+
+
+			stopApp(filtered_log)
+		})
+	}
+	runGadget(ui, server, viewer = dialogViewer("Filter activity instances based on presence of life cycle labels", height = 400))
+
+}
