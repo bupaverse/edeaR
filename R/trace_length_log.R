a48645f75bb8acd5da239d2defdea67ca67d0327
JANSSENSWILLEN Gert <gert.janssenswillen@uhasselt.be>
2019-01-08 14:25:12 +0100

    Fix bug trace_length_log

--- R/trace_length_log.R
+++ R/trace_length_log.R
@@ -6,7 +6,7 @@ trace_length_log <- function(eventlog) {
 	trace_length_case(eventlog) -> raw
 
 	raw %>%
-		pull(trace_length) %>%
+		pull(absolute) %>%
 		summary_statistics -> output
 
 	attr(output, "raw") <- raw
