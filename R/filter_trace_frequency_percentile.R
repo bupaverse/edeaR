a30f439b7f6d2634c63f484685dd7f97b5a5f8e1
Felix Mannhardt <felix.mannhardt@sintef.no>
2019-03-09 14:52:29 +0100

    Improve performance of `filter_trace_frequency` by avoiding to compute the `case_list` twice. Some duplication of code is the downside, but that could probably be refactored later.

--- R/filter_trace_frequency_percentile.R
+++ R/filter_trace_frequency_percentile.R
@@ -4,12 +4,25 @@ filter_trace_frequency_percentile <- function(eventlog,
 								  reverse){
 	cum_sum <- NULL
 	lag_cum_sum <- NULL
+	.N <- NULL
+	absolute_frequency <- NULL
+	relative_frequency <- NULL
 
-	eventlog %>%
-		case_list() -> cases
+	if(nrow(eventlog) == 0) {
+		return(filter_case(eventlog, c(), reverse))
+	}
 
-	eventlog %>%
-		trace_coverage("trace") %>%
+	cases <- case_list(eventlog)
+	data.table::setDT(cases)
+
+	traces <- cases[, .(absolute_frequency = .N), by = .(trace)]
+	traces <- traces[order(absolute_frequency, decreasing = T),
+					 relative_frequency:=absolute_frequency/sum(absolute_frequency)]
+
+	traces %>%
+		select(trace, absolute = absolute_frequency, relative = relative_frequency) %>%
+		arrange(desc(absolute)) %>%
+		mutate(cum_sum = cumsum(relative)) %>%
 		mutate(lag_cum_sum = lag(cum_sum, default = 0)) %>%
 		filter(lag_cum_sum <= percentage) %>%
 		merge(cases) %>%
