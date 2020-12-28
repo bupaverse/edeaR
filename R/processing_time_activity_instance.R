c23ee7f2003f933aa98d17fff96307b9eb47a1dc
DmitriBeloshitskiy <71018352+DmitriBeloshitskiy@users.noreply.github.com>
2020-12-28 02:29:04 +0100

    Implementing Cegeka modification

--- R/processing_time_activity_instance.R
+++ R/processing_time_activity_instance.R
@@ -23,43 +23,52 @@ processing_time_activity_instance <- function(eventlog,
 
 
 compute_processing_time <- function(start_dttm, end_dttm, work_schedule, units)  {
-
-	start_time <- NULL
-	end_time <- NULL
-	start_interval <- NULL
-	end_interval <- NULL
-	proc_hours <- NULL
-	work_hours <- NULL
-	difference <- NULL
-
-	if(is.null(work_schedule))
-		return(as.double(end_dttm - start_dttm, units = units))
-	else {
-		start_day <- lubridate::date(x = start_dttm)
-		end_day <- lubridate::date(x = end_dttm)
-
-		work_schedule %>%
-			filter(!is.na(start_time)) -> work_schedule
-		tibble(start_dttm,
-			   end_dttm,
-			   proc_hours = interval(start_dttm, end_dttm),
-			   date = zoo::as.Date(start_day:end_day)) %>%
-			mutate(day = ifelse(wday(date) == 1, 7, wday(date)-1)) %>%
-			inner_join(work_schedule, by = "day") %>%
-			filter(!is.na(start_time)) -> temp
-
-		if(nrow(temp) == 0) {
-			return(0)
-		}
-		 temp %>%
-			mutate(start_interval = ymd_hms(paste0(date, start_time))) %>%
-			mutate(end_interval = ymd_hms(paste0(date, end_time))) %>%
-			mutate(work_hours = interval(start_interval, end_interval)) %>%
-			mutate(difference = lubridate::intersect(proc_hours, work_hours)) %>%
-			filter(!is.na(difference)) %>%
-			summarize(processing_time = sum(as.double(difference, units = "hours"))) %>%
-			pull(processing_time)
-	}
+  
+  start_time <- NULL
+  end_time <- NULL
+  start_interval <- NULL
+  end_interval <- NULL
+  proc_hours <- NULL
+  work_hours <- NULL
+  difference <- NULL
+  
+  if(is.null(work_schedule))
+    return(as.double(end_dttm - start_dttm, units = units))
+  else {
+    start_day <- lubridate::date(x = start_dttm)
+    end_day <- lubridate::date(x = end_dttm)
+    work_schedule <-
+      work_schedule %>%
+      filter(!is.na(start_time))
+    temp <-
+      tibble(start_dttm,
+             end_dttm,
+             proc_hours = interval(start_dttm, end_dttm),
+             date = zoo::as.Date(start_day:end_day)) %>%
+      mutate(day = ifelse(wday(date) == 1, 7, wday(date)-1)) %>%
+      inner_join(work_schedule, by = "day") %>%
+      filter(!is.na(start_time))
+    
+    if(!exists("holidays")){
+      warning("Holidays are not accounted for. If you want to add holidays, create proper data object first. (Extra info @Dmitri)")
+    } else {
+      temp <-
+        temp %>% 
+        filter(!date %in% holidays$holiday_date)
+    }
+    
+    if(nrow(temp) == 0) {
+      return(0)
+    }
+    temp %>%
+      mutate(start_interval = ymd_hms(paste0(date, start_time))) %>%
+      mutate(end_interval = ymd_hms(paste0(date, end_time))) %>%
+      mutate(work_hours = interval(start_interval, end_interval)) %>%
+      mutate(difference = lubridate::intersect(proc_hours, work_hours)) %>%
+      filter(!is.na(difference)) %>%
+      summarize(processing_time = sum(as.double(difference, units = "hours"))) %>%
+      pull(processing_time)
+  }
 }
 
 
