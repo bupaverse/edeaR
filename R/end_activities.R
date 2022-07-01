3496f801a9659d5a0321b805fccc3453ee3b8d3d
Gerard van Hulzen <gerard.vanhulzen@uhasselt.be>
2022-07-01 15:08:35 +0200

    `start_activities()`

--- R/end_activities.R
+++ R/end_activities.R
@@ -2,40 +2,48 @@
 #'
 #' @description Analyse the end activities in the process.
 #'
-#' @param level \code{\link{character}} (default \code{"log"}): Level of granularity for the analysis: \code{"log"}, \code{"case"}, \code{"activity"}, \code{"resource"}, or \code{"resource-activity"}.
-#' For more information, see \code{vignette("metrics", "edeaR")} and 'Details' below.
+#' @param level \code{\link{character}} (default \code{"log"}): Level of granularity for the analysis: \code{"log"} (default),
+#' \code{"case"}, \code{"activity"}, \code{"resource"}, or \code{"resource-activity"}. For more information,
+#' see \code{vignette("metrics", "edeaR")} and 'Details' below.
 #'
 #' @details
 #' Argument \code{level} has the following options:
 #' \itemize{
-#' \item At \code{log} level, this metric shows the absolute and relative number of activities that are the last activity in one or more of the cases.
+#' \item At \code{log} level, this metric shows the absolute and relative number of activities that are the last activity
+#' in one or more of the cases.
 #' \item On \code{case} level, this metric provides an overview of the end activity of each case.
-#' \item On \code{activity} level, this metric calculates for each activity the absolute and relative number of cases that end with this activity type.
-#' Similar to the \code{\link{start_activities}} metric, the relative number is calculated as a portion of the number of cases, being the number of "opportunities" that an activity could be the end activity.
+#' \item On \code{activity} level, this metric calculates for each activity the absolute and relative number of cases that
+#' end with this activity type. Similar to the \code{\link{start_activities}} metric, the relative number is calculated
+#' as a portion of the number of cases, being the number of "opportunities" that an activity could be the end activity.
 #' The cumulative sum is added to have an insight in the number of activities that is required to cover a certain part of the total.
-#' \item At \code{resource} level, an overview of which resources execute the last activity per case can be of interest for a company.
-#' Probably this person is responsible for the correct communication to the customer.
-#' \item On \code{resource-activity} level, this metric shows for each occurring resource-activity combination the absolute and relative number of times
-#' this resource executes this activity as an end activity in a case.
+#' \item At \code{resource} level, an overview of which resources execute the last activity per case is provided.
+#' \item On \code{resource-activity} level, this metric shows for each occurring resource-activity combination the absolute
+#' and relative number of times this resource executes this activity as an end activity in a case.
 #' }
 #'
 #' @inherit activity_frequency params references return seealso
 #'
+#' @seealso \code{\link{start_activities}}
+#'
 #' @family metrics
 #'
 #' @export end_activities
-end_activities <- function(log, level = c("log", "case", "activity", "resource", "resource-activity"), append = deprecated(), append_column = NULL, sort = TRUE, ..., eventlog = deprecated())  {
+end_activities <- function(log,
+						   level = c("log", "case", "activity", "resource", "resource-activity"),
+						   append = deprecated(),
+						   append_column = NULL,
+						   sort = TRUE,
+						   eventlog = deprecated())  {
 	UseMethod("end_activities")
 }
 
-#' @describeIn end_activities Compute end activities for an \code{\link[bupaR]{eventlog}}.
+#' @describeIn end_activities Computes the end activities for an \code{\link[bupaR]{eventlog}}.
 #' @export
 end_activities.eventlog <- function(log,
 									level = c("log", "case", "activity", "resource", "resource-activity"),
 									append = deprecated(),
 									append_column = NULL,
 									sort = TRUE,
-									...,
 									eventlog = deprecated()) {
 
 	log <- lifecycle_warning_eventlog(log, eventlog)
@@ -67,14 +75,13 @@ end_activities.eventlog <- function(log,
 	return_metric(log, output, level, append, append_column, "end_activities", n_result_col = ifelse(level == "case",1,3))
 }
 
-#' @describeIn end_activities Compute end activities for a \code{\link[bupaR]{grouped_eventlog}}.
+#' @describeIn end_activities Computes the end activities for a \code{\link[bupaR]{grouped_eventlog}}.
 #' @export
 end_activities.grouped_eventlog <- function(log,
 											level = c("log", "case", "activity", "resource", "resource-activity"),
 											append = deprecated(),
 											append_column = NULL,
 											sort = TRUE,
-											...,
 											eventlog = deprecated()) {
 
 	log <- lifecycle_warning_eventlog(log, eventlog)
@@ -106,36 +113,42 @@ end_activities.grouped_eventlog <- function(log,
 	return_metric(log, output, level, append, append_column, "end_activities", n_result_col = ifelse(level == "case",1,3))
 }
 
-#' @describeIn end_activities Compute end activities for an \code{\link[bupaR]{activitylog}}.
+#' @describeIn end_activities Computes the end activities for an \code{\link[bupaR]{activitylog}}.
 #' @export
 end_activities.activitylog <- function(log,
 									   level = c("log", "case", "activity", "resource", "resource-activity"),
 									   append = deprecated(),
 									   append_column = NULL,
 									   sort = TRUE,
-									   ...,
 									   eventlog = deprecated()) {
 
 	log <- lifecycle_warning_eventlog(log, eventlog)
 	append <- lifecycle_warning_append(append)
 	level <- rlang::arg_match(level)
 
-	end_activities.eventlog(bupaR::to_eventlog(log), level = level, append = append, append_column = append_column, sort = sort, ...)
+	end_activities.eventlog(bupaR::to_eventlog(log),
+							level = level,
+							append = append,
+							append_column = append_column,
+							sort = sort)
 }
 
-#' @describeIn end_activities Compute end activities for a \code{\link[bupaR]{grouped_activitylog}}.
+#' @describeIn end_activities Computes the end activities for a \code{\link[bupaR]{grouped_activitylog}}.
 #' @export
 end_activities.grouped_activitylog <- function(log,
 											   level = c("log", "case", "activity", "resource", "resource-activity"),
 											   append = deprecated(),
 											   append_column = NULL,
 											   sort = TRUE,
-											   ...,
 											   eventlog = deprecated()) {
 
 	log <- lifecycle_warning_eventlog(log, eventlog)
 	append <- lifecycle_warning_append(append)
 	level <- rlang::arg_match(level)
 
-	end_activities.grouped_eventlog(bupaR::to_eventlog(log), level = level, append = append, append_column = append_column, sort = sort, ...)
+	end_activities.grouped_eventlog(bupaR::to_eventlog(log),
+									level = level,
+									append = append,
+									append_column = append_column,
+									sort = sort)
 }
--- R/start_activities_resource_activity.R
+++ R/start_activities_resource_activity.R
@@ -1,19 +1,20 @@
 
-start_activities_resource_activity <- function(eventlog) {
+start_activities_resource_activity <- function(log) {
+
 	absolute <- NULL
 	relative <- NULL
-	eventlog %>%
+
+	log %>%
 		as.data.frame() %>%
-		group_by(!!case_id_(eventlog)) %>%
-		arrange(!!as.symbol(timestamp(eventlog)), .order) %>%
-		summarize(!!as.symbol(activity_id(eventlog)) := first(!!as.symbol(activity_id(eventlog))),
-				  !!as.symbol(resource_id(eventlog)) := first(!!as.symbol(resource_id(eventlog)))) %>%
-		group_by(!!as.symbol(resource_id(eventlog)), !!as.symbol(activity_id(eventlog))) %>%
+		group_by(!!case_id_(log)) %>%
+		arrange(!!as.symbol(timestamp(log)), .order) %>%
+		summarize(!!as.symbol(activity_id(log)) := first(!!as.symbol(activity_id(log))),
+				  !!as.symbol(resource_id(log)) := first(!!as.symbol(resource_id(log)))) %>%
+		group_by(!!as.symbol(resource_id(log)), !!as.symbol(activity_id(log))) %>%
 		summarize(absolute = n()) %>%
 		ungroup() %>%
 		arrange(desc(absolute)) %>%
-		mutate(relative = absolute/n_cases(eventlog),
+		mutate(relative = absolute/n_cases(log),
 			   cum_sum = cumsum(relative)) %>%
-		arrange(!!resource_id_(eventlog), !!activity_id_(eventlog))
-
+		arrange(!!resource_id_(log), !!activity_id_(log))
 }
