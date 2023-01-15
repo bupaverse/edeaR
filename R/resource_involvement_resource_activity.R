04aa14219199f68c78a78e67b2445fdf1302aa31
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2023-01-15 20:48:23 +0100

    fix resource involvement resource-activity level

--- R/resource_involvement_resource_activity.R
+++ R/resource_involvement_resource_activity.R
@@ -7,7 +7,7 @@ resource_involvement_resource_activity <- function(log) {
 
 	log %>%
 		distinct(!!resource_id_(log), !!activity_id_(log), !!case_id_(log)) %>%
-		group_by(!!resource_id_(log)) %>%
+		group_by(!!resource_id_(log), !!activity_id_(log)) %>%
 		summarize("absolute" = n_distinct(!!case_id_(log))) %>%
 		ungroup() %>%
 		mutate(relative = absolute/n_cases(log))
